8eacf3d6d52ab1e7bd49479e61883020e660f274
Cristian Tala <yomismo@cristiantala.cl>
2017-10-31 12:29:38 +0100

    NameSpacing

--- classes/BareError.php
+++ classes/BareError.php
@@ -24,7 +24,7 @@
  * THE SOFTWARE.
  */
 
-namespace classes;
+namespace ctala\Baremetrics;
 
 /**
  * Description of BareError
--- classes/BareMetrics.php
+++ classes/BareMetrics.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace classes;
+namespace ctala\Baremetrics;
 
 /*
  * The MIT License
